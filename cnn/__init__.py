6aff8eea93dbe6c4f88e6c33781cf8c1206516ba
Alexis Roche <alexis.roche@gmail.com>
2018-03-20 11:10:36 +0000

    Directly import some cython routines

--- cnn/__init__.py
+++ cnn/__init__.py
@@ -1,5 +1,13 @@
 from .image_classifier import *
+from ._fastrun import (_convolve_image,
+                       _multi_convolve_image,
+                       _relu_max_pool_image,
+                       _get_device_info,
+                       _basic_test1d,
+                       _gpu_convolve_image,
+                       _gpu_multi_convolve_image)
 
+                       
 from numpy.testing import Tester
 test = Tester().test
 bench = Tester().bench
